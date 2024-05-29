fab45067a392bccba268ae6eda1fa2705bb5d8dc
Dan Behrman <dab@dab.la>
2024-05-28 22:47:55 -0400

    first working blog example

--- autogen/init_modules.go
+++ autogen/init_modules.go
@@ -9,8 +9,8 @@ import (
 
 func GetModules(app *core.App) map[string]core.Module {
 	modules := map[string]core.Module{}
+	modules["github.com/lightningsdk/ui"] = ui.NewModule(app)
 	modules["github.com/lightningsdk/blog"] = blog.NewModule(app)
 	modules["github.com/lightningsdk/db"] = db.NewModule(app)
-	modules["github.com/lightningsdk/ui"] = ui.NewModule(app)
 	return modules
 }
